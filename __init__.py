7765cdb97313ee60d363d630ba1723fefaefb2c0
Alexey-t <support@uvviewsoft.com>
2019-07-14 15:50:25 +0300

    dont use JsonComment

--- __init__.py
+++ __init__.py
@@ -1,11 +1,13 @@
+import re
 import json
-from .jsoncomment import JsonComment
 from .html_format import *
 from cuda_fmt import get_config_filename
 
 def format(text):
+
     fn = get_config_filename('HTML Beautify')
-    with open(fn) as f:
-        parser = JsonComment(json)
-        d = parser.loads(f.read())
+    s = open(fn, 'r').read()
+    #del // comments
+    s = re.sub(r'(^|[^:])//.*', r'\1', s)
+    d = json.loads(s)
     return do_html_format(text, d)
